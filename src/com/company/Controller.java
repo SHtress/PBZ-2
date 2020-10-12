a8137d9c637ad97e9c3c6dca5cb90d2d1aecafb3
SHtress <stas.butrin@mail.ru>
2020-10-12 13:07:20 +0300

    Add storage controller

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
         this.conn=conn;
     }
 
-    void AddingInventory(String name_inventory, String type_inventory) {
+    void addingInventory(String name_inventory, String type_inventory) {
         PreparedStatement addingInventoryStatement = null;
         try {
             addingInventoryStatement = conn.prepareStatement("INSERT INTO inventory(name_inventory, type_inventory) VALUES (?, ?)");
@@ -21,7 +21,7 @@ public class Controller {
         }
     }
 
-    void UpdatingInventory(String old_name_inventory, String old_type_inventory, String new_name_inventory, String new_type_inventory) {
+    void updatingInventory(String old_name_inventory, String old_type_inventory, String new_name_inventory, String new_type_inventory) {
         PreparedStatement updatingInventoryStatement = null;
         try {
             updatingInventoryStatement = conn.prepareStatement("UPDATE inventory SET name_inventory = ? , type_inventory = ? WHERE name_inventory = ? && type_inventory = ?");
@@ -35,7 +35,7 @@ public class Controller {
         }
     }
 
-    void DeletingInventory(String name_inventory, String type_inventory) {
+    void deletingInventory(String name_inventory, String type_inventory) {
         PreparedStatement deletingInventoryStatement = null;
         try {
             deletingInventoryStatement = conn.prepareStatement("DELETE FROM inventory WHERE name_inventory = ? && type_inventory = ?");
@@ -47,7 +47,7 @@ public class Controller {
         }
     }
 
-    void ShowingInventory() {
+    void showingInventory() {
         PreparedStatement preparedStatement = null;
         try {
             preparedStatement = conn.prepareStatement("SELECT * FROM inventory");
@@ -61,5 +61,64 @@ public class Controller {
             throwables.printStackTrace();
         }
 
+    }
+
+    void addingStorage(int storage_number, String storage_name, int storage_phone) {
+        PreparedStatement addingStorageStatement = null;
+        try {
+            addingStorageStatement = conn.prepareStatement("INSERT INTO storage(storage_number, storage_name, storage_phone) VALUES (?,?,?)");
+            addingStorageStatement.setInt(1, storage_number);
+            addingStorageStatement.setString(2, storage_name);
+            addingStorageStatement.setInt(3, storage_phone);
+            addingStorageStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    void updatingStorage(int old_storage_number, String old_storage_name, int old_storage_phone ,int new_storage_number, String new_storage_name, int new_storage_phone) {
+        PreparedStatement updatingStorageStatement = null;
+        try {
+            updatingStorageStatement = conn.prepareStatement("UPDATE storage SET storage_number = ?, storage_name = ?, storage_phone = ? WHERE storage_number = ? AND storage_name = ? AND storage_phone = ?");
+            updatingStorageStatement.setInt(1, new_storage_number);
+            updatingStorageStatement.setString(2, new_storage_name);
+            updatingStorageStatement.setInt(3, new_storage_phone);
+            updatingStorageStatement.setInt(4, old_storage_number);
+            updatingStorageStatement.setString(5, old_storage_name);
+            updatingStorageStatement.setInt(6, old_storage_phone);
+            updatingStorageStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+   void deletingStorage(int storage_number, String storage_name, int storage_phone) {
+       PreparedStatement updatingStorageStatement = null;
+       try {
+           updatingStorageStatement = conn.prepareStatement("DELETE FROM storage WHERE storage_number = ? AND storage_name = ? AND storage_phone = ?");
+           updatingStorageStatement.setInt(1, storage_number);
+           updatingStorageStatement.setString(2, storage_name);
+           updatingStorageStatement.setInt(3, storage_phone);
+           updatingStorageStatement.executeUpdate();
+       } catch (SQLException throwables) {
+           throwables.printStackTrace();
+       }
+   }
+
+    void showingStorageTable() {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = conn.prepareStatement("SELECT * FROM storage");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            int storage_number = resultSet.getInt("storage_number");
+            String storage_name = resultSet.getString("storage_name");
+            int storage_phone = resultSet.getInt("storage_phone");
+            System.out.printf(storage_number + " " + storage_name + " " + storage_phone + "\n");
+        }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import com.mysql.cj.exceptions.ConnectionIsClosedException;
+//import com.mysql.cj.exceptions.ConnectionIsClosedException;
 
 import java.sql.*;
 import java.util.Scanner;
@@ -10,53 +10,17 @@ public class Main {
         try {
             String url = "jdbc:mysql://localhost/laba2?serverTimezone=Europe/Moscow&useSSL=false";
             String username = "root";
-            String password = "1234";
+            String password = "";
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection conn = DriverManager.getConnection(url, username, password)) {
                 Controller controller = new Controller(conn);
                 //controller.AddingInventory("Грабли","Инструмент");
                 //controller.UpdatingInventory("Инструмент", "Инструмент", "Лопата", "Инструмент");
-                controller.DeletingInventory("","Спецодежда");
-                controller.ShowingInventory();
-                //----------- Adding storage --------------------------------------//
-//                int id_storage = 36;
-//                String name_storage = "Красный";
-//                int phone_storage = 890;
-//                PreparedStatement addingStorageStatement = conn.prepareStatement("INSERT INTO storage(id_storage, name_storage, phone_storage) VALUES (?,?,?)");
-//                addingStorageStatement.setInt(1, id_storage);
-//                addingStorageStatement.setString(2, name_storage);
-//                addingStorageStatement.setInt(3, phone_storage);
-//                addingStorageStatement.executeUpdate();
+                //controller.DeletingInventory("","Спецодежда");
+                //controller.ShowingInventory();
 
-                //----------- Updating storage --------------------------------------//
-//                int id_storage = 36;
-//                String name_storage = "Красный";
-//                int phone_storage = 890;
-//                PreparedStatement updatingStorageStatement = conn.prepareStatement("UPDATE storage SET id_storage = ? WHERE id_storage = ?");
-//                updatingStorageStatement.setInt(1, id_storage);
-//                updatingStorageStatement.setString(2, name_storage);
-//                updatingStorageStatement.setInt(3, phone_storage);
-//                updatingStorageStatement.executeUpdate();
+                controller.addingStorage(2,"BASE",653);
 
-                //----------- Deleting storage --------------------------------------//
-//                int id_storage = 36;
-//                String name_storage = "Красный";
-//                int phone_storage = 890;
-//                PreparedStatement updatingStorageStatement = conn.prepareStatement("DELETE FROM storage WHERE id_storage = ?");
-//                updatingStorageStatement.setInt(1, id_storage);
-//                updatingStorageStatement.setString(2, name_storage);
-//                updatingStorageStatement.setInt(3, phone_storage);
-//                updatingStorageStatement.executeUpdate();
-
-                //------------Showing storage table---------------------------------------//
-//                PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM storage");
-//                ResultSet resultSet = preparedStatement.executeQuery();
-//                while (resultSet.next()) {
-//                    int id_storage = resultSet.getInt("id_storage");
-//                    String name_storage = resultSet.getString("name_storage");
-//                    int phone_storage = resultSet.getInt("phone_storage");
-//                    System.out.printf(id_storage + " " + name_storage + " " + phone_storage + "\n");
-//                }
             }
         } catch (Exception ex) {
             System.out.println("Connection failed...");
